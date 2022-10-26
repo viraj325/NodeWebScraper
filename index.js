7b2a1fe82b672c6391afa8fc5e935855ce0e3592
Viraj Patel <virajpatel325@gmail.com>
2022-10-26 01:46:00 -0400

    making some progress

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/got_scraping', (req, res) => {
+    runOnlyGotScraping('https://washington.startups-list.com/')
+    res.send('done')
+})
+
 app.get('/example', (req, res) => {
-    parseFunction('https://news.ycombinator.com/').then(r => {
+    //'https://news.ycombinator.com/'
+    parseFunction('https://washington.startups-list.com/').then(r => {
         console.log(r)
         res.send(r)
     })
@@ -33,13 +39,22 @@ const parseFunction = async (url) => {
     // Use Cheerio to parse the HTML
     const $ = cheerio.load(html);
     // Select all the elements with the class name "athing"
-    const entries = $('.athing');
+    //const entries = $('.athing');
+    const entries = $('.card.startup');
+    let fel = entries.children().first(); // div
+    let lel = entries.children().last(); // a
+    console.log("TagNames Below")
+    //console.log(fel);
+    //console.log(lel);
+    console.log(fel.get(0).tagName);
+    console.log(lel.get(0).tagName);
+    console.log("TagNames End")
     const elementTextList = []
     // Loop through the entries
     for (const entry of entries) {
         const element = $(entry);
         // Write each element's text to the terminal
-        console.log(element.text());
+        //console.log(element.text());
         elementTextList.push(element.text())
     }
 
